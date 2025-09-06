aac6ad637018579ed38f7360adb602cc7fcc9af4
mauricio-isc <mauriciosan0315@gmail.com>
2025-09-05 20:12:15 -0600

    se anadio urls.py

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    #Podemos agregar campos personalizados
+    phone = models.CharField(max_length=15, blank=True)
+
+
+    def __str__(self):
+        return self.username
