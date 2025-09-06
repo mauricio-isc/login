e2d3f1b838af75cae31835c71893d252bf2eeff9
mauricio-isc <mauriciosan0315@gmail.com>
2025-09-05 20:08:28 -0600

    se anadio codigo a views.py

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth import login, logout
+from .serializers import UserSerializer, LoginSerializer
 # Create your views here.
+
+api_view(['POST'])
+@permission_classes([AllowAny])
+def login_view(request):
+    serializer = LoginSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.validate_data['user']
+        login(request, user)
+        return Response(UserSerializer(user).data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout_view(request):
+    logout(request)
+    return Response({"message": "Sesion cerrada correctamente"})
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_view(request):
+    return Response(UserSerializer(request.user).data)
