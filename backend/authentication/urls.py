368de7caa969e5aa0bb8b5b896da36c0b656684e
mauricio-isc <mauriciosan0315@gmail.com>
2025-09-09 17:34:26 -0600

    se a modificado urls.py

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('user/', views.user_view, name='user'),
+    path('register/', views.register_view, name='register'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
